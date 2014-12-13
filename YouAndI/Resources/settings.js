cffab71891439a09d2aee4f645bb229829ee63b8
Shehla Rana <shehla.saleem@gmail.com>
2014-12-12 22:22:47 -0800

    adding checks and inserting row to lover tables

--- YouAndI/Resources/settings.js
+++ YouAndI/Resources/settings.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////
 // Adding text field
-
+var AWS = require("ti.aws");
 var namelbl = Ti.UI.createLabel({
 	color:'blue',
   text: 'Name',
@@ -70,7 +70,7 @@ var button = Ti.UI.createButton({
 	height: 50
 });
 
-function add_a_lover(AWS, table_name, lover_details)
+function add_a_lover(table_name, lover_details)
 {
 	var params = {
 			'RequestJSON' : {
@@ -99,7 +99,7 @@ function add_a_lover(AWS, table_name, lover_details)
 	});	
 }
 
-function create_lovers_table(AWS, table_name)
+function create_lovers_table(table_name)
 {
 	AWS.DDB.createTable({
         "RequestJSON" : {
@@ -130,23 +130,55 @@ function create_lovers_table(AWS, table_name)
     });	
 }
 
-function fetch_lover_details(AWS, phone)
+function check_fields()
+{
+	if(nametxt.value == "")
+	{
+		alert('Can\'t register without a name');
+		return false;
+	}
+	if(phonetxt.value == "")
+	{
+		alert("Please enter a phone number to register");
+		return false;
+	}
+	return true;
+	
+}
+
+function handle_lover_found(response)
+{
+	response["data"]["Responses"]["lovers"]["Items"][0]["name"]["S"];
+	alert(nametxt.value+': you love '+ JSON.stringify(response["data"]["Responses"]["lovers"]["Items"][0]["name"]["S"])+ '?');
+	Ti.API.info(JSON.stringify(response));
+	lover_details = {
+		'dummy': 1,
+		'id': guid(),
+		'name': nametxt.value,
+		'phone': phonetxt.value
+	};
+	alert('writing '+lover_details['id']+' '+lover_details['name']+' '+lover_details['phone']);
+	add_a_lover("lovers", lover_details);	
+}
+
+function handle_no_lover_found(error)
+{
+	alert(nametxt.value+': ooh, it seems your lover has not registered yet :(');	
+	Ti.API.info(JSON.stringify(error));	
+}
+
+function fetch_lover_details(phone)
 {	
-	var params = '{"RequestJSON" : {"RequestItems":{"lovers": {"Keys": [{"HashKeyElement": {"N":"1"}, "RangeKeyElement":{"S":"'+phone+'"}}],"AttributesToGet":["name"]}}}}';
+	var params = '{"RequestJSON" : {"RequestItems":{"lovers": {"Keys": [{"HashKeyElement": {"N":"1"}, "RangeKeyElement":{"S":"'+phone+'"}}],"AttributesToGet":["name", "id"]}}}}';
+	
 			
 	AWS.DDB.batchGetItem(JSON.parse(params),
 			
 		function(data, response) {
-		alert(nametxt.value+': you love '+ JSON.stringify(response["data"]["Responses"]["lovers"]["Items"][0]["name"]["S"])+ '?');
-		Ti.API.info(JSON.stringify(response));
-
-  	},  function(message,error) {
-		//alert('Error: '+ JSON.stringify(error));
-		alert(nametxt.value+': ooh, it seems your lover has not registered yet :(')
-		Ti.API.info(JSON.stringify(error));
-
-	});	
-	
+		handle_lover_found(response);
+  	},  function(message,error) {				
+		handle_no_lover_found(error);
+	});			
 }	
 
 var guid = (function() {
@@ -161,21 +193,14 @@ var guid = (function() {
   };
 })();
 
-button.addEventListener('click', function(e){
-	Ti.API.info('Cool :');
-	var AWS = require("ti.aws");
+button.addEventListener('click', function(e){	
 	AWS.authorize('AKIAIX5POYG6QZNZHRVA', 'UTyP0FYscLPhdAukw+kBw+Y8hNMLNw4GLjmEQ5y3');
 	//create_lovers_table(AWS, "lovers");
-	lover_details = {
-		'dummy': 1,
-		'id': guid(),
-		'name': nametxt.value,
-		'phone': phonetxt.value
-	};
-	//add_a_lover(AWS, "lovers", lover_details);
-	fetch_lover_details(AWS, lover_phone_txt.value);
 	
-	Ti.API.info('RAD!!!');
+	if (check_fields())
+	{		
+		fetch_lover_details(lover_phone_txt.value);		
+	}		
 });
 
 Ti.UI.currentWindow.add(button);
